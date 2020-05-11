91e819ec161c306d5de903b1e4bb743ace1fed47
presenty1897 <presenty1897@gmail.com>
2020-05-11 20:53:04 +0900

    add move snake

--- server.js
+++ server.js
@@ -1,32 +1,67 @@
-//app.use(express.static(path.join(__dirname, 'node_modules')));
-
 var WebsockerServer = require("ws").Server;
 var wss = new WebsockerServer({port: 8888});
 var time = 0;
 
+var simulator = {
+    snakes: [{
+        head_direction: 0, // direction
+        head_pos: {
+            x: 5,
+            y: 5
+        },
+        body:[0, 0, 0, 0] // directions or item id
+    }],
+    moveSnake: function(){
+        this.snakes.forEach(snake => {
+            switch(snake.head_direction){
+                case 0: // up
+                    snake.head_pos.y -= 1;
+                    break;
+                case 1: // down
+                    snake.head_pos.y -= 1;
+                    break;
+                case 2: // left
+                    snake.head_pos.x -= 1;
+                    break;
+                case 3: // right
+                    snake.head_pos.x -= 1;
+                    break;
+                default:
+                    throw new Error("없는 방향입니다.");
+            };
+        })
+    }
+    
+};
+
 var sender = {
     ws_list: [],
-    sendText:function(text){
+    sendText:function(obj){
         var ws_list = this.ws_list;
         ws_list.forEach(ws => {
-            ws.send(JSON.stringify(text));
+            ws.send(JSON.stringify(obj));
         });
     }
 };
 
 function sendData(){
-    sender.sendText(time);
-    time++;
+    sender.sendText(simulator.snakes[0].head_pos);
 };
 
-setInterval(sendData, 100);
+function steppingTime(){
+    simulator.moveSnake();
+    sendData();
+    time++;
+}
+
+setInterval(steppingTime, 100);
 
 console.log("server started");
 wss.on('connection', (ws, req) => {
     console.log(req);
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(ip + "아이피의 클라이언트에서 접속 요청이 발생했습니다.");
-    if(typeof sender.ws_list.find(element => element == ws) === "undefined"){
+    if(sender.ws_list.indexOf(element => element == ws) === -1){
         sender.ws_list.push(ws);
         console.log("ws was added!!");
     }
